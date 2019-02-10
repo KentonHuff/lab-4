c2feca7a6df9b47dd4dfe31be6f40aced2e7e5ed
Kenton Huff <kentonahuff@gmail.com>
2019-02-10 13:48:10 -0600

    Added outline and documentation of ZooTest

--- ZooTest.java
+++ ZooTest.java
@@ -6,5 +6,75 @@
  */
 public class ZooTest
 {
+	/**
+	 * Test the Zoo constructor and getters
+	 */
+	public void fullConstructorTest() throws AssertException
+	{
+		
+	}
 	
+	/**
+	 * Test the expandZoo method
+	 */
+	public void expandZooTest() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the addAnimal method
+	 */
+	public void addAnimalTest() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the empty getTotalHeight method
+	 */
+	public void getTotalHeight() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the getAverageWeight method
+	 */
+	public void getAverageHeight() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the getAverageWeight with color parameter
+	 */
+	public void getAverageWeightColor() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the getAnimals method
+	 */
+	public void getAnimalsTest() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the getCapacity method
+	 */
+	public void getCapacityTest() throws AssertException
+	{
+		
+	}
+	
+	/**
+	 * Test the toString method
+	 */
+	public void toStringTest() throws AssertException
+	{
+		
+	}
 }
